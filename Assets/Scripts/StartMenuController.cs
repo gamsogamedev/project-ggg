8b807713f33a6e99bf0c1d8bc4fd5ea6d29bfb62
An Unusual Pug <rafaelzzanon64@gmail.com>
2026-06-23 02:18:26 -0300

    Mudanças de menu (detalhes nesses três pontos)
    Adicionado sons para hover e clique de botões
    Reposicionado música do combate para o menu
    Adicionado fonte diferente de texto
    Adicionado botão customizado para o menu

--- Assets/Scripts/StartMenuController.cs
+++ Assets/Scripts/StartMenuController.cs
@@ -7,24 +7,28 @@ public class StartMenuController : MonoBehaviour
 {
 
     public Animator transition;
+    public AudioSource menuSoundSource;
+    public AudioClip toBattleSound;
+    [Range(0f, 1f)] public float toBattleVolume = 1f;
     public float transitionTime = 1f;
+    public float soundTime = 2f;
 
     public void OnStartClick()
     {
         StartCoroutine(LoadLevel("Combate"));
     }
 
-    public void ToMenuClick()
+    /*public void ToMenuClick()
     {
         StartCoroutine(LoadLevel("Menu"));
-    }
+    }*/
 
     IEnumerator LoadLevel(string cena)
     {
+        if (menuSoundSource != null && toBattleSound != null) menuSoundSource.PlayOneShot(toBattleSound, toBattleVolume);
         transition.SetTrigger("Start");
-
+        yield return new WaitForSeconds(soundTime);
         yield return new WaitForSeconds(transitionTime);
-
         SceneManager.LoadScene(cena);
     }
 
