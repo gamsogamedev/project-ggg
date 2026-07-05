b2de505e26e76c6923b62369d39d234b7f4878f2
An Unusual Pug <rafaelzzanon64@gmail.com>
2026-07-04 21:48:53 -0300

    Adicionado menu de configs funcional
    (testar via build o fullscreen)

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -6,9 +6,44 @@ using UnityEngine.EventSystems;
 public class MenuManager : MonoBehaviour
 {
     public GameObject _mainMenuFirst;
+    public GameObject _optionsFirst;
+
+    [SerializeField] private GameObject _mainMenuGO;
+    [SerializeField] private GameObject _optionsMenuGO;
 
     private void Start()
     {
+        _mainMenuGO.SetActive(true);
+        _optionsMenuGO.SetActive(false);
         EventSystem.current.SetSelectedGameObject(_mainMenuFirst);
+    }
+
+    public void OpenMainMenuHandle()
+    {
+        _mainMenuGO.SetActive(true);
+        _optionsMenuGO.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(_mainMenuFirst);
+    }
+
+    public void onOptionsPress()
+    {
+        OpenOptionsMenuHandle();
+    }
+
+    public void OpenOptionsMenuHandle()
+    {
+        _mainMenuGO.SetActive(false);
+        _optionsMenuGO.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(_optionsFirst);
+    }
+
+    public void onOptionsBackPress()
+    {
+        OpenMainMenuHandle();
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
